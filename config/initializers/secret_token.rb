516f91ef094ea18392648f8726263efa2332a7fa
James Hart <hjhart@gmail.com>
2011-05-29 13:12:47 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieQueue::Application.config.secret_token = '183ec47f0041bb8b2ea5b4f94775de939c814d4ed40a8979e09593b9317653bd2a1904c3ee910ea6d0d92085b514b7ef0ec93db5ef842d1ecc01ab8aa471d6e0'
